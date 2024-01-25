e5952b79eddeb9f2d58f47a45495cca87de6f8c5
Blanca Aguado López <130659305+blancaal96@users.noreply.github.com>
2024-01-25 16:38:27 +0100

    Add files via upload

---
+++ Task Competition/start.m
@@ -0,0 +1,37 @@
+%% EEG-Competition Task (main.m)
+% -------------------------------------------------------------------------
+% CIMCYC - Universidad de Granada  - Code written by Paloma Díaz Gutiérrez and Chema G. Peñalver
+% Adapted by Blanca Aguado López
+% -------------------------------------------------------------------------
+
+clc
+
+%% 1. EXPERIMENT INITIALIZATION
+% ----------------------------------------------------------  
+run src\session.m           % Session initialization
+run src\conf.m              % Experiment configuration
+run src\ctb.m               % Counterbalance configuration
+run src\init2.m             % Experiment initialization
+run src\ptb.m
+
+%% 2. INSTRUCTIONS
+% ---------------------------------------------------------- 
+run src\instructions.m
+
+%% 3. PRACTICE
+% ----------------------------------------------------------
+ run src\practiceInst.m
+ run src\practice.m
+
+%% 4. EXPERIMENT
+% ----------------------------------------------------------
+run src\experimentalInst.m
+run src\experimental.m
+
+%% 5. SAVE DATA
+% ---------------------------------------------------------
+run src\saveworkspace.m     % Save output data 
+
+%% 5. GOODBYE
+% ---------------------------------------------------------
+run src\goodbye.m           % Goodbye!      
